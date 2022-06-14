a4259ab299015e08940b0d939bdb95a6c1dbc4d1
Wiji <peawseaw@gmail.com>
2022-06-13 22:37:09 -0700

    create css /use grid and flex

--- components/Programmer.js
+++ components/Programmer.js
@@ -1,24 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from '@/styles/Programmer.module.css'
 
 export default function Programmer({pgrm}) {
     return (
         <div>
-            <div>
-                <Image src={pgrm.image ? pgrm.image : 'programmer Image'} 
-                width = {280}
-                height={300}/>
-            </div>
-            <h1>{pgrm.name}</h1>
-            <p>{pgrm.location}</p>
-            <h4>{pgrm.description}</h4>
-            <Link href={`https://github.com/${pgrm.githubId}`}>
+            <Image src={pgrm.image ? pgrm.image : 'programmer Image'} width = {280} height={300}/>
+                <h1>{pgrm.name}</h1>
+                <p>{pgrm.location}</p>
+                <h4>{pgrm.description}</h4>
+            <div className={styles.groupLink}>
+                <Link href={`https://github.com/${pgrm.githubId}`}>
                     <a>Github</a>
-            </Link>
-            <Link href={`www.linkedin.com/in/${pgrm.linkedInId}`}>
+                </Link>
+                <Link href={`www.linkedin.com/in/${pgrm.linkedInId}`}>
                     <a>LinkedIn</a>
-            </Link>
-
+                </Link>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
 import Programmer from '@/components/Programmer'
+import styles from "@/styles/Index.module.css"
 
 export default function HomePage({programmers}) {
   return (
     <Layout>
       {programmers.length === 0 && <h3>No programmers to show</h3>}
-
+      <div className={styles.container}>
         {programmers.map((pgrm) => (
-        <Programmer key={pgrm.id} pgrm={pgrm}/>
-      ))}
-
+          <Programmer key={pgrm.id} pgrm={pgrm}/>
+        ))}
+      </div>
     </Layout>
   )
 }
