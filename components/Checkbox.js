a326d2f9ec5d0a29e72ebe6ed5e9eeaeec2960b2
Wiji <peawseaw@gmail.com>
2022-06-17 23:28:58 -0700

    add checkbox input

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -5,8 +5,17 @@ export default function Checkbox() {
 
     return (
         <div className={styles.container}>
-            <CheckboxInput name={"React Developer"} />
+            <CheckboxInput name={"AWS Solutions Architect"} />
+            <CheckboxInput name={"Back-end Developer"} />
+            <CheckboxInput name={"C# Developer"} />
+            <CheckboxInput name={"Data Engineer"} />
+            <CheckboxInput name={"Front-end Developer"} />
+            <CheckboxInput name={"Full Stack Developer"} />
+            <CheckboxInput name={"Java Developer"} />
+            <CheckboxInput name={"JavaScript Developer"} />
             <CheckboxInput name={"Python Developer"} />
+            <CheckboxInput name={"React Developer"} />
+            <CheckboxInput name={"Web Developer"} />
         </div>
     )
 }
